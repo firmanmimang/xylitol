0d28eb7eae4560204bc76aedc3bafaf67d7eef2e
firmanmimang <fhidayat131@gmail.com>
2022-09-30 16:32:15 +0700

    add connection db setting staging

--- connections/lottexylitolsmile.id.php
+++ connections/lottexylitolsmile.id.php
@@ -1,2 +1,4 @@
 <?php  $rs = array(0=> array('dbname' => '2022_lottexyl_xy', 'dbusername' => 'root', 'dbpass' => '' ) ); ?>
+<!-- staging -->
+<?php // $rs = array(0=> array('dbname' => '2022_lottexylitolsmile', 'dbusername' => 'aleale', 'dbpass' => '!z|hjbLX?O%tL4<Fw' ) ); ?>
 <?php // $rs = array(0=> array('dbname' => 'lottexyl_xy1', 'dbusername' => 'lottexyl_xy1', 'dbpass' => '!PS+WRWWDPlh' ) ); ?>
