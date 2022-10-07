859be9e2947b79bd0bf72168afc04ef0821efe83
firmanmimang <fhidayat131@gmail.com>
2022-10-07 16:07:32 +0700

    bypass user not active when login

--- ajax-member.php
+++ ajax-member.php
@@ -162,13 +162,13 @@ $loginLog = new LoginLog();
 								 
 								}
 								
-								else if ($result[0]['statuskey'] == 1){
-									$class->addErrorList($arrReturn,false,$class->errorMsg['login'][1]);
-								}
+								// else if ($result[0]['statuskey'] == 1){
+								// 	$class->addErrorList($arrReturn,false,$class->errorMsg['login'][1]);
+								// }
 								else if ($result[0]['statuskey'] == 3){
 									$class->addErrorList($arrReturn,false,$class->errorMsg['login'][2]);
 								}
-								else if ($result[0]['statuskey'] == 2){
+								else if ($result[0]['statuskey'] == 2 || $result[0]['statuskey'] == 1){
 									 
 										$_SESSION[$class->loginSession]['id'] = base64_encode($result[0]['pkey']);
 										$_SESSION[$class->loginSession]['name'] = $result[0]['name']; 
