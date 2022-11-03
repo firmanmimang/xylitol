cc1ffc433e8d548b5a001ca29bbbde0eafc0382a
firmanmimang <fhidayat131@gmail.com>
2022-11-03 13:31:53 +0700

    tambah email blas asdf-reminder1-ff dan _function_smtp_echo_ff file

---
+++ _function_smtp_echo_ff.php
@@ -0,0 +1,51 @@
+<?php
+require __DIR__.'/PHPMailer/PHPMailer.php';
+require __DIR__.'/PHPMailer/SMTP.php';
+require __DIR__.'/PHPMailer/Exception.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+function smtp_mail($to, $subject, $message, $from, $debug = false)
+{
+    $mail = new PHPMailer();
+    $mail->SMTPDebug = $debug;
+    $mail->CharSet = "UTF-8";
+    $mail->isSMTP();
+
+    // hapus semua tujuan, cc, dan bcc
+    $mail->clearAddresses();
+    $mail->clearCCs();
+    $mail->clearBCCs();
+
+    // konfigurasi dasar SMTP
+    $mail->SMTPAuth = "true";
+    $mail->SMTPSecure = "tls";
+
+    $mail->Host = "lottexylitolsmile.id;103.30.247.50";
+    $mail->Port = "587";
+
+    $mail->Username = "no-reply@lottexylitolsmile.id";
+    $mail->Password = "";
+
+    $default_email_from = "no-reply@lottexylitolsmile.id";
+
+    if(empty($from)) $mail->setFrom($default_email_from);
+    else $mail->setFrom($from);
+    
+    $mail->Subject = $subject;
+    
+    $mail->Body = $message;
+    $mail->IsHTML(true);
+
+    $mail->addAddress($to);
+    
+    if ($mail->Send()) {
+        echo $to ." Email Sent..! <br>";
+    } else {
+        echo $to ." Error..! <br>";
+    }
+
+    $mail->smtpClose();
+}
---
+++ asdf-reminder1-ff.php
@@ -0,0 +1,38 @@
+<?php 
+require_once '_config.php'; 
+require_once '_include-fe-v2.php';
+require_once '_global.php'; 
+require __DIR__.'/_function_smtp_echo_ff.php';
+
+includeClass("Customer.class.php");
+
+sendBlastEmail();
+    
+echo 'done';
+
+function sendBlastEmail(){
+        global $twig;
+        global $class;
+        
+        $customer = new Customer();
+        $rsCustomer = $customer->searchDataRow(array('email'),' and point < 20 and statuskey <> 3 and statuskey <> 1');
+
+        // nanti jadikan default variable
+        $arrTwigVar = array();
+        $arrTwigVar = $class->getDefaultEmailVariable(); 
+         
+        $twig->render('email-template.html');  
+        $content = $twig->render('email-reminder-phase1-ff.html', $arrTwigVar);
+
+        $rsCustomer = [
+            ['email'=> 'fhidayat131@gmail.com',],
+            ['email'=> 'firman.hidayat@concretejakarta.com',],
+        ];
+
+        foreach($rsCustomer as $row){
+            $email = $row['email']; 
+            echo $email;
+            // smtp_mail($email, 'Reminder Pengundian Phase 1', $content, '');
+        }		 
+}
+?>
